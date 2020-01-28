1cdaf46a02736067bce9e456dc2ae07e88562dbf
LYHGabe <YHLee@robotics.snu.ac.kr>
2020-01-28 14:24:49 +0900

    cuda 0

--- Libraries/.ipynb_checkpoints/ICNN_optim_boundary_to_Xstable-checkpoint.py
+++ Libraries/.ipynb_checkpoints/ICNN_optim_boundary_to_Xstable-checkpoint.py
@@ -37,10 +37,12 @@ class Grid_dataset(Dataset):
     
 class ICNN_optim:
     
-    def __init__(self,V_hidden_sizes, fhat_hidden_sizes,tol,alpha,Xstable):
+    def __init__(self,V_hidden_sizes, fhat_hidden_sizes,tol,alpha,Xstable,max_speed):
         super(ICNN_optim, self).__init__()
         
         self.model = ICNN_net.ICNN_net( V_hidden_sizes, fhat_hidden_sizes,tol,alpha)
+        
+        self.max_speed = max_speed
         self.Xstable = Xstable
     def get_grid(self,nq1,nq2,qmin,qmax):
         self.nq1 = nq1
@@ -74,29 +76,19 @@ class ICNN_optim:
         q_in_mesh[:,:,0] = q1_mesh
         q_in_mesh[:,:,1] = q2_mesh
         
-        
         #getting boundary grid
-        q_in_boundary = torch.zeros((nq1_b*2+nq2_b*2),2,device=device, dtype=dtype)
+        q_in_boundary = torch.zeros((nq1_b*2+nq2_b*2-4),2,device=device, dtype=dtype)
         q_in_boundary[0:nq1_b,:] = q_in_mesh[0,:,:]
-        q_in_boundary[0:nq1_b,0] = qmin[0]
-        q_in_boundary[nq1_b:nq1_b+nq2_b,:] = q_in_mesh[:,-1,:]
-        q_in_boundary[nq1_b:nq1_b+nq2_b,1] = qmax[1]
-        q_in_boundary[nq1_b+nq2_b:nq1_b+2*nq2_b,:] = q_in_mesh[:,0,:]
-        q_in_boundary[nq1_b+nq2_b:nq1_b+2*nq2_b,1] = qmin[1]
-        q_in_boundary[nq1_b+2*nq2_b:,:] = q_in_mesh[-1,:,:]
-        q_in_boundary[nq1_b+2*nq2_b:,0] = qmax[0]
-        
-        
+        q_in_boundary[nq1_b:nq1_b+nq2_b-1,:] = q_in_mesh[1:,-1,:]
+        q_in_boundary[nq1_b+nq2_b-1:nq1_b+2*nq2_b-3,:] = q_in_mesh[1:-1,0,:]
+        q_in_boundary[nq1_b+2*nq2_b-3:,:] = q_in_mesh[-1,:-1,:]
         #asigning q_dot to the boundary data
-        boundary_speed = 1.0
-        q_dot_boundary  = torch.zeros((nq1_b*2+nq2_b*2),2,device=device, dtype=dtype)
-        q_dot_boundary[0:nq1_b,0] = 1
-        q_dot_boundary[nq1_b:nq1_b+nq2_b,1] = -1
-        q_dot_boundary[nq1_b+nq2_b:nq1_b+2*nq2_b,1] = 1
-        q_dot_boundary[nq1_b+2*nq2_b:,0] = -1
-        
+        boundary_speed = 1.0*self.max_speed
+        to_Xstable = self.Xstable-q_in_boundary
+        norm_to_Xstable = torch.sqrt(((to_Xstable**2)[:,0] + (to_Xstable**2)[:,1])).view(-1,1)
+        q_dot_boundary = (boundary_speed*(to_Xstable)/norm_to_Xstable).to(device)
         q_in_boundary.requires_grad=True
-        return q_in_boundary,q_dot_boundary
+        return q_in_boundary,q_dot_boundary,q1,q2
 
     def get_grid2(self,nq,qmin,qmax): #not using it yet..
 
@@ -137,37 +129,32 @@ class ICNN_optim:
         q_in.requires_grad=True
         qdot_real = q_dot.t().to(device)
         loss_fn = torch.nn.MSELoss(reduction='sum')
-        #loss_mat = torch.zeros(epoch,dtype=dtype,device=device)
+        loss_mat = torch.zeros(epoch,dtype=dtype,device=device)
         for t in range(epoch):
             
             qdot_bound_pred = self.model.f_forward(q_in_boundary,self.Xstable).to(device)
-            temp0_boundary = torch.sqrt((qdot_bound_pred**2)[:,0]+(qdot_bound_pred**2)[:,1])
+            #loss_boundary = math.pi*2/(q_in_boundary.shape[0])*loss_fn(q_dot_boundary,qdot_bound_pred).to(device)
             temp_boundary = torch.mul(q_dot_boundary,qdot_bound_pred)
-            temp2_boundary = torch.sum(F.relu(-temp_boundary[:,0]-temp_boundary[:,1])/temp0_boundary)# -는 relu 쓰기위함
-            loss_boundary = math.pi*4/(q_in_boundary.shape[0])*temp2_boundary 
-            penalty_boundary = 1
+            temp2_boundary = torch.sum(temp_boundary[:,0]+temp_boundary[:,1])
+            loss_boundary = math.pi*4/(q_in_boundary.shape[0])*(-temp2_boundary) # -는 relu 쓰기위함
             
-            
+            penalty_boundary = 200
             qdot_pred = self.model.f_forward(q_in,self.Xstable).to(device)
             loss_traj = dt*loss_fn(qdot_pred,qdot_real).to(device)
-            
-            loss = loss_traj+penalty_boundary*((loss_boundary)**2)
+            loss = loss_traj+penalty_boundary*(F.relu(loss_boundary)**2)
             loss.backward(retain_graph=True)
             optimizer.step()
             q_in.grad.zero_()
-            q_in_boundary.grad.zero_()
             self.model.zero_grad()
             optimizer.zero_grad()
             ld = loss.data.clone().to(device_c)
-            #loss_mat[t] = loss_boundary.data
-            print ('\r epoch = '+str(t+1)+ ', loss = '
-                       +str(ld.data.numpy()) +', loss_b = '+str(loss_boundary.to(device_c).data.numpy())+ 
-                   ', loss_t = '+str(loss_traj.to(device_c).data.numpy())+'                    .', end = ' ')
-            
-            if t>50000:
+            loss_mat[t] = loss.data
+            print ('\r epoch = '+str(t+1)+ ', loss = '+str(ld.data.numpy()), end = ' ')
+            if t>5000:
                 if (loss_mat[t]-loss_mat[t-1000])/loss_mat[t] < 0.001:
                     print('Optimization finished')
                     break
+        return loss_mat
         
     def optim_Euc_reg(self,q_in_reg,qtraj ,q_dot,dt,q_in_boundary,q_dot_boundary, learning_rate = 1e-2,epoch=10000,
                       batch_size = 400):
@@ -178,10 +165,9 @@ class ICNN_optim:
                                   shuffle=True,
                                   num_workers=8)
         
-        
         if batch_size>q_in_reg.shape[0]:
-            #print('batch_size is reduced to maximum batch_size = '+str(q_in_reg.shape[0])+'.')
-            batch_size = q_in_reg.shape[0]
+            print('batch_size is reduced to maximum batch_size = '+str(q_in_reg.shape[0])+'.')
+        batch_size = q_in_reg.shape[0]
         optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
         q_in = qtraj.t().to(device)
         q_in.requires_grad=True
@@ -190,46 +176,31 @@ class ICNN_optim:
         
         
         loss_fn = torch.nn.MSELoss(reduction='sum')
-        #loss_mat = torch.zeros(epoch,dtype=dtype,device=device)
+        loss_mat = torch.zeros(epoch,dtype=dtype,device=device)
         for t in range(epoch):
             for i, data in enumerate(train_loader):
-                
-                qdot_bound_pred = self.model.f_forward(q_in_boundary,self.Xstable).to(device)
-                temp0_boundary = torch.sqrt((qdot_bound_pred**2)[:,0]+(qdot_bound_pred**2)[:,1])
-                temp_boundary = torch.mul(q_dot_boundary,qdot_bound_pred)
-                temp2_boundary = torch.sum(F.relu(-temp_boundary[:,0]-temp_boundary[:,1])/temp0_boundary)# -는 relu 쓰기위함
-                
-                loss_boundary = math.pi*4/(q_in_boundary.shape[0])*temp2_boundary 
-                penalty_boundary = 1000
-                
-                
-                
                 data0 = data[0]
                 data1 = data[1]
                 current_grid_data = torch.zeros(data[0].shape[0],2, device=device, dtype=dtype)
                 current_grid_data[:,0]  = data[0][:,0].clone()
                 current_grid_data[:,1]  = data[1][:,0].clone()
                 current_grid_data.requires_grad=True
-                
                 current_output = self.model.f_forward(current_grid_data,self.Xstable).to(device)
 
                 co1 = torch.sum(current_output[:,0])
                 co2 = torch.sum(current_output[:,1])
-                
                 qdot_grad1 = grad(co1,current_grid_data,create_graph=True,retain_graph=True)[0]
                 qdot_grad2 = grad(co2,current_grid_data,create_graph=True,retain_graph=True)[0]
 
                 qdot_grad_norm = qdot_grad1**2+qdot_grad2**2
-                #print(self.nq1*self.nq2, q_in_reg.shape[0])
                 loss_reg = torch.sum(qdot_grad_norm)*self.dA/2*(self.nq1*self.nq2/batch_size)
+
 
                 qdot_pred = self.model.f_forward(q_in,self.Xstable).to(device)
                 loss_task = dt*loss_fn(qdot_pred,qdot_real).to(device)
-                
-                penalty = 1000
+                penalty = 200
                 eps = 0.01
-                loss = loss_reg+penalty*(F.relu(loss_task-eps)**2)+penalty_boundary*((loss_boundary)**2)
-                
+                loss = loss_reg+penalty*(F.relu(loss_task-eps)**2)
                 #print(loss)
                 loss.backward(retain_graph=True)
                 optimizer.step()
@@ -237,37 +208,17 @@ class ICNN_optim:
                 self.model.zero_grad()
                 optimizer.zero_grad()
                 ld = loss.data.clone().to(device_c)
-                #loss_mat[t] = loss.data
-                if i%10==0:
-                    total_grid = q_in_reg.to(device)
-                    total_grid.requires_grad=True
-                    current_output = self.model.f_forward(total_grid,self.Xstable).to(device)
-
-                    co1 = torch.sum(current_output[:,0])
-                    co2 = torch.sum(current_output[:,1])
-
-                    qdot_grad1 = grad(co1,total_grid,create_graph=True,retain_graph=True)[0]
-                    qdot_grad2 = grad(co2,total_grid,create_graph=True,retain_graph=True)[0]
-
-                    qdot_grad_norm = qdot_grad1**2+qdot_grad2**2
-                    total_loss_reg = torch.sum(qdot_grad_norm)*self.dA/2
-                    total_loss = (total_loss_reg+penalty*(F.relu(loss_task-eps)**2)+penalty_boundary*((loss_boundary)**2)).data.clone().to(device_c)
-                    
-                print ('\r epoch = '+str(t) +' i = '+str(i+1)+ 
-                       ', loss = ' +str(ld.data.numpy())+
-                       ', total_loss = ' +str(total_loss.data.numpy())+
-                       ', loss_b = '+str(loss_boundary.to(device_c).data.numpy())+ 
-                       ', loss_t = '+str(loss_task.to(device_c).data.numpy())+
-                       ', loss_reg = '+str(loss_reg.to(device_c).data.numpy())+
-                       ', loss_reg_total =  '+str(total_loss_reg.to(device_c).data.numpy())+'           .',end = ' ')
-                
-                if t>10000:
+                loss_mat[t] = loss.data
+                print ('\r epoch = '+str(t) +' i = '+str(i+1)+ ', loss = '
+                       +str(ld.data.numpy()), end = ' ')
+                if t>5000:
                     if (loss_mat[t]-loss_mat[t-300])/loss_mat[t] < 0.001:
                         print('Optimization finished')
-            
+                        return loss_mat   
+        return loss_mat            
         
         
-    def optim_Kinematic_reg(self, robot, q_in_reg, qtraj, q_dot, dt,q_in_boundary,q_dot_boundary, Xstable, learning_rate = 1e-3, epoch=10000,
+    def optim_Kinematic_reg(self, robot, q_in_reg, qtraj, q_dot, dt, Xstable, learning_rate = 1e-3, epoch=10000,
                       batch_size = 400):
         
         dataset = Grid_dataset(q_in_reg.numpy())
@@ -275,27 +226,16 @@ class ICNN_optim:
                                           batch_size=batch_size,
                                           shuffle=True,
                                           num_workers=8)
-        if batch_size>q_in_reg.shape[0]:
-            #print('batch_size is reduced to maximum batch_size = '+str(q_in_reg.shape[0])+'.')
-            batch_size = q_in_reg.shape[0]
-            
+        batch_size = q_in_reg.shape[0]
         optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
         q_in = qtraj.t().to(device)
         q_in.requires_grad=True
         qdot_real = q_dot.t().to(device)
         loss_fn = torch.nn.MSELoss(reduction='sum')
-        #loss_mat = torch.zeros(epoch,dtype=dtype,device=device)
+        loss_mat = torch.zeros(epoch,dtype=dtype,device=device)
 
         for t in range(epoch):
             for i, data in enumerate(train_loader):
-                
-                qdot_bound_pred = self.model.f_forward(q_in_boundary,self.Xstable).to(device)
-                temp0_boundary = torch.sqrt((qdot_bound_pred**2)[:,0]+(qdot_bound_pred**2)[:,1])
-                temp_boundary = torch.mul(q_dot_boundary,qdot_bound_pred)
-                temp2_boundary = torch.sum(F.relu(-temp_boundary[:,0]-temp_boundary[:,1])/temp0_boundary)# -는 relu 쓰기위함
-                
-                loss_boundary = math.pi*4/(q_in_boundary.shape[0])*temp2_boundary 
-                penalty_boundary = 1000
 
                 data0 = data[0]
                 data1 = data[1]
@@ -340,11 +280,12 @@ class ICNN_optim:
 
                 loss_reg = Integral_approximation*self.dA/2*(self.nq1*self.nq2/batch_size)
 
+
                 qdot_pred = self.model.f_forward(q_in,Xstable).to(device)
                 loss_task = dt*loss_fn(qdot_pred,qdot_real).to(device)
                 penalty = 1000
-                eps = 0.01
-                loss = loss_reg+penalty*(F.relu(loss_task-eps)**2)+penalty_boundary*((loss_boundary)**2)
+                eps = 0.0001
+                loss = loss_reg+penalty*(F.relu(loss_task-eps)**2)
                 #print(loss)
                 loss.backward(retain_graph=True)
                 optimizer.step()
@@ -352,41 +293,15 @@ class ICNN_optim:
                 self.model.zero_grad()
                 optimizer.zero_grad()
                 ld = loss.data.clone().to(device_c)
-                #loss_mat[t] = loss.data
-                if i%10==0:
-                    total_grid = q_in_reg.to(device)
-                    total_grid.requires_grad=True
-                    current_output = self.model.f_forward(total_grid,self.Xstable).to(device)
-
-                    co1 = torch.sum(current_output[:,0])
-                    co2 = torch.sum(current_output[:,1])
-
-                    qdot_grad1 = grad(co1,total_grid,create_graph=True,retain_graph=True)[0]
-                    qdot_grad2 = grad(co2,total_grid,create_graph=True,retain_graph=True)[0]
-
-                    qdot_grad_norm = qdot_grad1**2+qdot_grad2**2
-                    ##################
-                    #??????????????????????????? this should be modified !!!!!!!!!!!!!!!
-                    ##################
-                    ###################
-                    ############################
-                    ##############
-                    total_loss_reg = torch.sum(qdot_grad_norm)*self.dA/2
-                    total_loss = (total_loss_reg+penalty*(F.relu(loss_task-eps)**2)+penalty_boundary*((loss_boundary)**2)).data.clone().to(device_c)
-                    
-                print ('\r epoch = '+str(t) +' i = '+str(i+1)+ 
-                       ', loss = ' +str(ld.data.numpy())+
-                       ', total_loss = ' +str(total_loss.data.numpy())+
-                       ', loss_b = '+str(loss_boundary.to(device_c).data.numpy())+ 
-                       ', loss_t = '+str(loss_task.to(device_c).data.numpy())+
-                       ', loss_reg = '+str(loss_reg.to(device_c).data.numpy())+
-                       ', loss_reg_total =  '+str(total_loss_reg.to(device_c).data.numpy())+'           .',end = ' ')
+                loss_mat[t] = loss.data
+                print ('\r epoch = '+str(t) +' i = '+str(i+1)+ ', loss = '
+                       +str(ld.data.numpy()), end = ' ')
                 if t>10000:
                     if (loss_mat[t]-loss_mat[t-100])/loss_mat[t] < 0.00001:
                         print('Optimization finished')
                         
         
-    def optim_Kinetic_reg(self, robot, q_in_reg, qtraj, q_dot,dt,q_in_boundary,q_dot_boundary, Xstable, learning_rate = 1e-3, epoch=10000,
+    def optim_Kinetic_reg(self, robot, q_in_reg, qtraj, q_dot, dt, Xstable, learning_rate = 1e-3, epoch=10000,
                       batch_size = 400):
         
         dataset = Grid_dataset(q_in_reg.numpy())
@@ -394,28 +309,16 @@ class ICNN_optim:
                                           batch_size=batch_size,
                                           shuffle=True,
                                           num_workers=8)
-        if batch_size>q_in_reg.shape[0]:
-            #print('batch_size is reduced to maximum batch_size = '+str(q_in_reg.shape[0])+'.')
-            batch_size = q_in_reg.shape[0]
-            
-            
+        batch_size = q_in_reg.shape[0]
         optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
         q_in = qtraj.t().to(device)
         q_in.requires_grad=True
         qdot_real = q_dot.t().to(device)
         loss_fn = torch.nn.MSELoss(reduction='sum')
-        #loss_mat = torch.zeros(epoch,dtype=dtype,device=device)
+        loss_mat = torch.zeros(epoch,dtype=dtype,device=device)
 
         for t in range(epoch):
             for i, data in enumerate(train_loader):
-                
-                qdot_bound_pred = self.model.f_forward(q_in_boundary,self.Xstable).to(device)
-                temp0_boundary = torch.sqrt((qdot_bound_pred**2)[:,0]+(qdot_bound_pred**2)[:,1])
-                temp_boundary = torch.mul(q_dot_boundary,qdot_bound_pred)
-                temp2_boundary = torch.sum(F.relu(-temp_boundary[:,0]-temp_boundary[:,1])/temp0_boundary)# -는 relu 쓰기위함
-                
-                loss_boundary = math.pi*4/(q_in_boundary.shape[0])*temp2_boundary 
-                penalty_boundary = 1000
 
                 data0 = data[0]
                 data1 = data[1]
@@ -464,8 +367,8 @@ class ICNN_optim:
                 qdot_pred = self.model.f_forward(q_in,Xstable).to(device)
                 loss_task = dt*loss_fn(qdot_pred,qdot_real).to(device)
                 penalty = 1000
-                eps = 0.01
-                loss = loss_reg+penalty*(F.relu(loss_task-eps)**2)+penalty_boundary*((loss_boundary)**2)
+                eps = 0.0001
+                loss = loss_reg+penalty*(F.relu(loss_task-eps)**2)
                 #print(loss)
                 loss.backward(retain_graph=True)
                 optimizer.step()
@@ -473,30 +376,9 @@ class ICNN_optim:
                 self.model.zero_grad()
                 optimizer.zero_grad()
                 ld = loss.data.clone().to(device_c)
-                #loss_mat[t] = loss.data
-                
-                if i%10==0:
-                    total_grid = q_in_reg.to(device)
-                    total_grid.requires_grad=True
-                    current_output = self.model.f_forward(total_grid,self.Xstable).to(device)
-
-                    co1 = torch.sum(current_output[:,0])
-                    co2 = torch.sum(current_output[:,1])
-
-                    qdot_grad1 = grad(co1,total_grid,create_graph=True,retain_graph=True)[0]
-                    qdot_grad2 = grad(co2,total_grid,create_graph=True,retain_graph=True)[0]
-
-                    qdot_grad_norm = qdot_grad1**2+qdot_grad2**2
-                    total_loss_reg = torch.sum(qdot_grad_norm)*self.dA/2
-                    total_loss = (total_loss_reg+penalty*(F.relu(loss_task-eps)**2)+penalty_boundary*((loss_boundary)**2)).data.clone().to(device_c)
-                    
-                print ('\r epoch = '+str(t) +' i = '+str(i+1)+ 
-                       ', loss = ' +str(ld.data.numpy())+
-                       ', total_loss = ' +str(total_loss.data.numpy())+
-                       ', loss_b = '+str(loss_boundary.to(device_c).data.numpy())+ 
-                       ', loss_t = '+str(loss_task.to(device_c).data.numpy())+
-                       ', loss_reg = '+str(loss_reg.to(device_c).data.numpy())+
-                       ', loss_reg_total =  '+str(total_loss_reg.to(device_c).data.numpy())+'           .',end = ' ')
+                loss_mat[t] = loss.data
+                print ('\r epoch = '+str(t) +' i = '+str(i+1)+ ', loss = '
+                       +str(ld.data.numpy()), end = ' ')
                 if t>10000:
                     if (loss_mat[t]-loss_mat[t-100])/loss_mat[t] < 0.00001:
                         print('Optimization finished')
